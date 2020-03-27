7517584c4e2228d092e77411ef38e0ead8975240
Zachary T. Harmany <zharmany@gmail.com>
2020-03-27 14:27:53 -0700

    Some updates.

--- sample/core.py
+++ sample/core.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
-from . import helpers
+from .helpers import get_answer
+from .helpers import BaseClass1, BaseClass2
 
 def get_hmm():
     """Get a thought."""
     return 'hmmm...'
 
-
 def hmm():
     """Contemplation..."""
-    if helpers.get_answer():
+    if get_answer():
         print(get_hmm())
+
+BaseClasses = [BaseClass1, BaseClass2]
+
+class MyClass(*BaseClasses):
+    """This class relies on BaseClass helper methods."""
+
+    def message(self):
+        self.message1()
+        self.message2()
--- sample/helpers.py
+++ sample/helpers.py
@@ -1,3 +1,15 @@
 def get_answer():
     """Get an answer."""
     return True
+
+class BaseClass1:
+    """This class contains some helper methods."""
+
+    def message1(self):
+        print('This is from BaseClass1.')
+
+class BaseClass2:
+    """This class contains some more helper methods."""
+    
+    def message2(self):
+        print('This is from BaseClass2.')
